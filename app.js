cb2fc4a30fb6dae9b0fe93828314efd7b84691fc
riccardi <riccardi.ashley@gmail.com>
2016-05-02 18:35:29 -0400

    It works

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var express = require( 'express' );
-var swig = require( 'swig' );
+var express = require('express');
 var app = express(); // creates an instance of an express application
+var swig = require('swig');
 var server = app.listen(3000);
 var socketio = require('socket.io');
 var io = socketio.listen(server);
-
-
-
 var routes = require("./routes/");
+var pg = require('pg');
+var conString = 'postgres://localhost:5432/twitterdb';
+var client = new pg.Client(conString);
 
-app.use("/", routes(io));
+//connect to postgres
+client.connect();
+
+//logging middleware
+app.use("/", routes(io, client));
 app.use(express.static("public"));
 
 app.engine('html', swig.renderFile);
@@ -18,22 +22,21 @@ app.set('views', __dirname + '/views');
 swig.setDefaults({ cache: false });
 
 app.get('/', function(req, res) {
-	var people = [{name: 'Grace'}, {name: 'Hopper'}, {name: 'Ashley'}, {name: 'Kathy'}];
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-	//res.send('Hello World!');
+    var people = [{ name: 'Grace' }, { name: 'Hopper' }, { name: 'Ashley' }, { name: 'Kathy' }];
+    res.render('index', { title: 'Hall of Fame', people: people });
+    //res.send('Hello World!');
 });
 
 // in some file that is in the root directory of our application
 var locals = {
     title: 'An Example',
     people: [
-        { name: 'Gandalf'},
+        { name: 'Gandalf' },
         { name: 'Frodo' },
-        { name: 'Hermione'}
+        { name: 'Hermione' }
     ]
 };
 
-swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+swig.renderFile(__dirname + '/views/index.html', locals, function(err, output) {
     // console.log(output);
 });
-
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,69 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
 // could use one line instead: var router = require('express').Router();
+
+var bodyParser = require('body-parser');
 var tweetBank = require('../tweetBank');
-// parse application/json
-// app.use(bodyParser.json())
+
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
-module.exports = function (io) {
-  router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
- 
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
-});
+module.exports = function(io, client) {
+    router.get('/', function(req, res) {
+        //var tweets = tweetBank.list();
+        //res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+        client.query('SELECT * FROM tweets, users WHERE tweets.userid = users.id', function(err, result) {
+            var tweets = result.rows;
+            console.log(tweets);
+            res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+        });
+    });
 
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, name: name, showForm: true } );
-});
+    router.get('/users/:name', function(req, res) {
+        var name = Number(req.params.name);
+        // var list = tweetBank.find({ name: name });
+        // res.render('index', { title: 'Twitter.js - Posts by ' + name, tweets: list, name: name, showForm: true });
+        client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE userid=$1', [name], function(err, result) {
+            var tweets = result.rows;
+            res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+        });
+    });
 
-router.post('/tweets', urlencodedParser, function(req, res) {
-	if (!req.body) return res.sendStatus(400)
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  io.sockets.emit('new_tweet', { text: text, name:name });
+    router.post('/tweets', urlencodedParser, function(req, res) {
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        var name = req.body.name;
+        var text = req.body.text;
+        //tweetBank.add(name, text);
+        client.query('SELECT * FROM users WHERE name=$1', [name], function(err, result) {
+            if (result.rows.length > 0) {
+                var userId = result.rows[0].id;
+                var pic = result.rows[0].pictureurl;
+                client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [userId, text], function(err, data) {
+                    io.sockets.emit('new_tweet', { text: text, name: name, pictureurl: pic, userId: userId });
+                });
+            } else {
+                client.query('INSERT INTO users (name, pictureurl) VALUES ($1, $2)', [name, "https://placekitten.com/g/50/50"], function(err, data) {
+                    client.query('SELECT * FROM users WHERE name=$1', [name], function(err, result) {
+                        var userId = result.rows[0].id;
+                        var pic = result.rows[0].pictureurl;
+                        client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [userId, text], function(err, data) {
+                            io.sockets.emit('new_tweet', { text: text, name: name, pictureurl: pic, userId: userId });
+                        });
+                    });
+                });
+            }
+        });
 
-  res.redirect('/');
-});
+
+        //res.redirect('/');
+
+    });
 
 
-  return router;
+
+
+    return router;
 };
-
